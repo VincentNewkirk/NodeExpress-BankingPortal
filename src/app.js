074385fced565d3118017f0c313b6e8d5cf196ea
vincent newkirk <newkirk.vincent@metlife.co.jp>
2020-05-22 14:10:32 +0900

    Module 3

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.render('index',  { title: 'Account Summary', accounts });
@@ -35,6 +36,36 @@ app.get('/credit', function(req, res) {
 
 app.get('/profile', function(req, res) {
   res.render('profile', { user: users[0] });
+});
+
+app.get('/transfer', function(req, res) {
+  res.render('transfer');
+});
+
+app.post('/transfer', function(req, res) {
+  const { body: { from, to, amount } } = req;
+  accounts[from].balance -= parseInt(amount);
+  accounts[to].balance += parseInt(amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON,  'utf8');
+
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', function(req, res) {
+  res.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', function(req, res) {
+  const { body: { amount: amt } } = req;
+  const amount = parseInt(amt);
+  accounts.credit.balance -= amount;
+  accounts.credit.available += amount;
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+
 });
 
 app.listen(3000, function() {
