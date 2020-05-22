070818db415ae0bf6ce892e3cf2bd0bb2162d6bf
vincent newkirk <newkirk.vincent@metlife.co.jp>
2020-05-22 13:15:50 +0900

    Module 2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
-const fs = require*('fs');
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
+
+const jsonDir = path.join(__dirname, 'json');
+
+const accountData = fs.readFileSync(jsonDir + '/accounts.json', { encoding: 'UTF8' });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(jsonDir + '/users.json', { encoding: 'UTF8' });
+const users = JSON.parse(userData);
+
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
@@ -9,8 +18,24 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.render('index',  { title: 'Index' });
+  res.render('index',  { title: 'Account Summary', accounts });
 },);
+
+app.get('/savings', function(req, res) {
+  res.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', function(req, res) {
+  res.render('account', { account: accounts.checking });
+});
+
+app.get('/credit', function(req, res) {
+  res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', function(req, res) {
+  res.render('profile', { user: users[0] });
+});
 
 app.listen(3000, function() {
   console.log('PS Project Running on port 3000!');
